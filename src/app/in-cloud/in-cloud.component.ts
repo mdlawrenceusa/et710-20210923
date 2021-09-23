3d6ad20189208c313fb8e057e38006bd65f4ee59
mdlawrenceusa <mdlawrence@me.com>
2021-09-23 11:54:15 -0400

    1 file modified, 2 added

---
+++ src/app/in-cloud/in-cloud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-in-cloud',
+  templateUrl: './in-cloud.component.html',
+  styleUrls: ['./in-cloud.component.css']
+})
+export class InCloudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
